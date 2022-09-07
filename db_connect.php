d2012bd0a19cb886849520cbc5bd48701f449948
Henry1998N <93736837+Henry1998N@users.noreply.github.com>
2022-09-07 18:55:54 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php 
 /// connect to database
-$conn = mysqli_connect('localhost', 'henry', 'HENRY137$$','fproject');
+$conn = mysqli_connect('', '', '','');
 ///check connection
 if(!$conn){
 	echo 'Connection error : '.mysqli_connect_error();
